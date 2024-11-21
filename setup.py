2c033c9f76a6265a9a5f3f4a053fe0030aa5e207
Wesley Maa <wesley.maa@gmail.com>
2024-11-21 00:59:20 -0800

    more 3.8

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 """Setup script for the project."""
 
 import re
+from typing import List
 
 from setuptools import setup
 
@@ -11,11 +12,11 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 
 with open("kinfer/requirements.txt", "r", encoding="utf-8") as f:
-    requirements: list[str] = f.read().splitlines()
+    requirements: List[str] = f.read().splitlines()
 
 
 with open("kinfer/requirements-dev.txt", "r", encoding="utf-8") as f:
-    requirements_dev: list[str] = f.read().splitlines()
+    requirements_dev: List[str] = f.read().splitlines()
 
 
 with open("kinfer/__init__.py", "r", encoding="utf-8") as fh:
